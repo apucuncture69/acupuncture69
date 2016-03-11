ecb95cbba78b5ec0acd10ce7f212f0dc2887f542
Robin Collinet <robin.collinet@cpe.fr>
2016-03-11 17:29:44 +0100

    maj Pathologie

--- model/acu/Pathologie.php
+++ model/acu/Pathologie.php
@@ -1,13 +1,17 @@
 <?php
 
+/* Classe de représentation d'une pathologie
+   Auteur : Robin */
+
 class Pathologie
 {
 	private $_meridien;		// acu.Meridien
 	private $_type;			// String
 	private $_description;		// String
 	private $_symptomes;		// List<acu.Symptome> : liste des symptômes associés à la pathologie
-	
-	public function __construct($data = NULL)
+
+	/* Constructeur */		
+	public function __construct(array $data = NULL)
 	{
 		if ($data)
 		{
@@ -15,5 +19,61 @@ class Pathologie
 		}
 	}
 
+	/* Getters */
+
 	public function getMeridien()
 	{
+		return $this->_meridien;
+	}
+
+	public function getType()
+	{
+		return $this->_type;
+	}
+
+	public function getDescription()
+	{
+		return $this->_description;
+	}
+
+	public function getSymptomes()
+	{
+		return $this->_symptomes;
+	}
+
+	/* Setters */
+
+	public function setMeridien($meridien)
+	{
+		$this->_meridien = $meridien;
+	}
+
+	public function setType($type)
+	{
+		$this->_type = $type;
+	}
+
+	public function setDescription($description)
+	{
+		$this->_description = $description;
+	}
+
+	public function setSymptomes($symptomes)
+	{
+		$this->_symptomes = $symptomes;
+	}
+
+	/* Hydratation */
+
+	public function hydrate(array $data)
+	{
+		foreach ($data as $key=>$value)
+		{
+			$method = 'set'.ucfirst($key);
+
+			if (method_exists($this, $method)
+			{
+				$this->$method($value);
+			}
+		}
+	}
