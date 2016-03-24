464b777a2f5b92f5c948e809b3b59727762c027d
DucViet <nicolas.brat@etu.univ-lyon1.fr>
2016-03-24 14:13:31 +0100

    Controller erreur

---
+++ controller/Error.php
@@ -0,0 +1,19 @@
+<?php
+
+require("libs/Smarty.class.php");
+
+class Error
+{
+	private $smarty;
+
+	public function __construct()
+	{
+		$this->smarty = new Smarty();
+	}
+
+	public function error_404()
+  {
+  	
+  }
+}
+?>
