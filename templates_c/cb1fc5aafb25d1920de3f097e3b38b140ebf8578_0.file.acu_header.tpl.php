9c0ac11ab6d13924800a53b8cccab639cadace09
BRETON Michael <michaelneuf@yahoo.fr>
2016-03-24 14:08:24 +0100

    Change repository

--- templates_c/cb1fc5aafb25d1920de3f097e3b38b140ebf8578_0.file.acu_header.tpl.php
+++ templates_c/cb1fc5aafb25d1920de3f097e3b38b140ebf8578_0.file.acu_header.tpl.php
@@ -1,17 +1,17 @@
 <?php
-/* Smarty version 3.1.28, created on 2016-03-24 13:46:37
+/* Smarty version 3.1.28, created on 2016-03-24 14:06:59
   from "C:\Program Files\wamp\www\acupuncture\view\acu_header.tpl" */
 
 if ($_smarty_tpl->smarty->ext->_validateCompiled->decodeProperties($_smarty_tpl, array (
   'has_nocache_code' => false,
   'version' => '3.1.28',
-  'unifunc' => 'content_56f3e1ad2ad385_97984924',
+  'unifunc' => 'content_56f3e673917d85_89742398',
   'file_dependency' => 
   array (
     'cb1fc5aafb25d1920de3f097e3b38b140ebf8578' => 
     array (
       0 => 'C:\\Program Files\\wamp\\www\\acupuncture\\view\\acu_header.tpl',
-      1 => 1458823535,
+      1 => 1458824425,
       2 => 'file',
     ),
   ),
@@ -19,7 +19,7 @@ if ($_smarty_tpl->smarty->ext->_validateCompiled->decodeProperties($_smarty_tpl,
   array (
   ),
 ),false)) {
-function content_56f3e1ad2ad385_97984924 ($_smarty_tpl) {
+function content_56f3e673917d85_89742398 ($_smarty_tpl) {
 ?>
 <header>
   <a href="./">
@@ -37,6 +37,11 @@ function content_56f3e1ad2ad385_97984924 ($_smarty_tpl) {
       <img id="header_profile_avatar" src="http://www.bakefly.com/uploads/section/avatar.jpg" alt='Image de profil' />
       <p id="header_profile_username">Michael Breton</p>
     </a>
+    <ul>
+      <li>Menu 1</li>
+      <li>Menu 2</li>
+      <li>Deconnexion</li>
+    </ul>
   </div>
 </header>
 <?php }
