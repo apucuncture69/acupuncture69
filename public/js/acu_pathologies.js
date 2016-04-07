c7d9035e69fa8186570ca2edab9fc94a5e65a81c
DucViet <nicolas.brat@etu.univ-lyon1.fr>
2016-04-07 17:04:37 +0200

    Update acu_pathologies.js

--- public/js/acu_pathologies.js
+++ public/js/acu_pathologies.js
@@ -1,1 +1,21 @@
 
+$( document ).ready(function() {
+
+	$.ajax({
+	    url: 'templating',
+	    type: 'POST',
+	    data: {
+	    	method: 'GET',
+	    	url: 'api/pathologies',
+	    	tpl: 'acu_pathologies_data'
+	    },
+	    success: function(data) {
+	    	//alert(data);
+	    	$('#pathologies_content').html(data);
+	    },
+	    error: function(result) {
+	        alert('mince');
+	    }
+	});
+
+});
