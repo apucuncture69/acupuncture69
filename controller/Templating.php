4b518a201641bb443c9a6e9e9460f5694b8c813e
DucViet <nicolas.brat@etu.univ-lyon1.fr>
2016-04-07 17:20:01 +0200

    Update Templating.php

--- controller/Templating.php
+++ controller/Templating.php
@@ -12,27 +12,32 @@ class Templating
 
 	public function draw() {
 
-		$opts = array(
-			'http'=>array(
-				'method'=>$_POST['method'],
-				'header'=>'Content-type: application/x-www-form-urlencoded',
-			)
-		);
-		$context = stream_context_create($opts);
-		 
-		$response = file_get_contents(
-		               'http://'.$_SERVER['SERVER_NAME'].'/'.split("/",$_SERVER['REQUEST_URI'])[1].'/'.$_POST['url'], 
-		               false, 
-		               $context);
+		try {
+			$opts = array(
+				'http'=>array(
+					'method'=>$_POST['method'],
+					'header'=>'Content-type: application/x-www-form-urlencoded',
+				)
+			);
+			$context = stream_context_create($opts);
+			 
+			$response = file_get_contents(
+			               'http://'.$_SERVER['SERVER_NAME'].'/'.split("/",$_SERVER['REQUEST_URI'])[1].'/'.$_POST['url'], 
+			               false, 
+			               $context);
 
-		$response = preg_replace('/<\?xml.*\?>/i','',$response);
+			$response = preg_replace('/<\?xml.*\?>/i','',$response);
 
-		$xml = simplexml_load_string($response);
+			$xml = simplexml_load_string($response);
 
-		
-		$tpl = 'view/'.$_POST['tpl'].'.tpl';
-		$this->smarty->assign('smarty_xml', $xml);
-		$this->smarty->display($tpl);
+			
+			$tpl = 'view/'.$_POST['tpl'].'.tpl';
+			$this->smarty->assign('smarty_xml', $xml);
+			$this->smarty->display($tpl);
+
+		} catch(Exception $e) {
+			return null;
+		}
 		
 	}
 }
