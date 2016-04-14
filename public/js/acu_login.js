542ba834deaa34d7de456f5fde785ba66b72baae
DucViet <nicolas.brat@etu.univ-lyon1.fr>
2016-04-14 22:02:48 +0200

    Update acu_login.js

--- public/js/acu_login.js
+++ public/js/acu_login.js
@@ -1,15 +1,31 @@
 $( document ).ready(function() {
 
     $('#login_submit').click(function(){
+    	if($('#login_password').val().length < 5){
+			$('#login_password').addClass( 'elt_form_err' );
+		} else {
+			$('#login_password').removeClass( 'elt_form_err' );
+		}
+		if(/^[a-z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$/.test($('#login_email').val())){
+			$('#login_email').removeClass( 'elt_form_err' );
+		} else {
+			$('#login_email').addClass( 'elt_form_err' );
+		}
 		if($('#login_email').val().length > 0 && !$('#login_email').hasClass('elt_form_err')
 		&& $('#login_password').val().length > 0 && !$('#login_password').hasClass('elt_form_err')){
 			$.ajax({
 			    url: 'api/user',
 			    type: 'POST',
-			    data: {email: $('#login_email').val(), password: $.sha256($('#login_password').val())},
+			    data: {
+			    	email: $('#login_email').val(),
+			    	password: $.sha256($('#login_password').val()+$.sha256($('#login_email').val()))
+			    },
 			    success: function(result) {
 			        if(result==true){
 			        	document.location.href=$('#login_redirect_page').val();
+			        } else {
+			        	$('#login_email').addClass( 'elt_form_err' );
+			        	$('#login_password').addClass( 'elt_form_err' );
 			        }
 			    },
 			    error: function(result) {
@@ -24,9 +40,9 @@ $( document ).ready(function() {
 				$('#login_password').addClass( 'elt_form_err' );
 			}
 		}
-
+		
 	});
-//^[a-z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$
+
 	$('#login_email').keyup(function(){
 		if(/^[a-z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$/.test($('#login_email').val())){
 			$('#login_email').removeClass( 'elt_form_err' );
