f3b44c7503d2c38bbdd8ec0302794b8839bc034e
DucViet <nicolas.brat@etu.univ-lyon1.fr>
2016-04-16 01:04:53 +0200

    Create acu_infos.js

---
+++ public/js/acu_infos.js
@@ -0,0 +1,37 @@
+$( document ).ready(function() {
+
+	$('.acu_infos').hover(function(){
+		$('#login_password').addClass( 'elt_form_err' );
+		$('#login_password').removeClass( 'elt_form_err' );
+	});
+
+	$('.acu_infos').each(function( index ) {
+		var img = $(this).children( 'div.acu_infos_img' );
+		var txt = $(this).children( 'div.acu_infos_txt' ).children( 'p:first' );
+		img.hover(
+			function(){
+				txt.removeClass( 'acu_infos_userinfos' );
+				txt.addClass( 'acu_infos_userinfos_hover' );
+			},
+			function(){
+				txt.addClass( 'acu_infos_userinfos' );
+				txt.removeClass( 'acu_infos_userinfos_hover' );
+			}
+		);
+	});
+
+	$('.acu_infos_txt').each(function( index ) {
+		var txt = $(this).children( 'p:first' );
+		$(this).hover(
+			function(){
+				txt.removeClass( 'acu_infos_userinfos' );
+				txt.addClass( 'acu_infos_userinfos_hover' );
+			},
+			function(){
+				txt.addClass( 'acu_infos_userinfos' );
+				txt.removeClass( 'acu_infos_userinfos_hover' );
+			}
+		);
+	});
+
+});
