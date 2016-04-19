3f2a3656dcb2b7bb5073155ed8912b910a6a0cb8
DucViet <nicolas.brat@etu.univ-lyon1.fr>
2016-04-19 22:59:39 +0200

    Update WebServiceDefine.php

--- config/WebServiceDefine.php
+++ config/WebServiceDefine.php
@@ -2,4 +2,5 @@
  define ('ManagerFolderPath',"model/manager/");
  define ("ConnexionFolderPath","model/connexion/");
  define ("ModelFolderPath","model/acu/");
+ define ("UserFolderPath","model/user/");
  ?>
