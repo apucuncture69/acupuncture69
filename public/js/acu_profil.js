252c3410d2889067f275e2751dbac5d5b63c0ab4
DucViet <nicolas.brat@etu.univ-lyon1.fr>
2016-04-28 16:21:50 +0200

    Create acu_profil.js

---
+++ public/js/acu_profil.js
@@ -0,0 +1,36 @@
+$( document ).ready(function() {
+
+    $('#profil_submit').click(function(){
+    		var pwd = '';
+    		var newpwd = '';
+    		if($('#profil_newmdp').val()!=''){
+    			pwd=$.sha256($('#profil_mdp').val()+$.sha256($('#profil_email').val()));
+    			newpwd=$.sha256($('#profil_newmdp').val()+$.sha256($('#profil_email').val()));
+    		}
+			$.ajax({
+			    url: 'api/user',
+			    type: 'PUT',
+			    data: {
+			    	prenom: $('#profil_prenom').val(),
+			    	nom: $('#profil_nom').val(),
+			    	email: $.trim($('#profil_email').html()),
+			    	password: pwd,
+			    	newpassword: newpwd
+			    },
+			    success: function(result) {
+			        if(result==true){
+			        	document.location.href='profil-saved';
+			        } else {
+			        	$('#profil_mdp').addClass( 'elt_form_err' );
+			        	$('#profil_mdp').val('');
+			        	$('#profil_newmdp').val('');
+			        	$('#profil_error').html('Modifications non enregistrées, mauvais mot de passe.');
+			        }
+			    },
+			    error: function(result) {
+			        alert('mince');
+			    }
+			});
+	});
+
+});
