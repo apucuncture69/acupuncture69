3a3850a200de6872305744f16b2e527e76b0b9b0
rcol69 <robin-collinet@orange.fr>
2016-04-28 14:24:28 +0200

    Update UserService.php

--- service/UserService.php
+++ service/UserService.php
@@ -2,6 +2,7 @@
 require_once("config/WebServiceDefine.php");
 require_once(ManagerFolderPath."UsersManager.php");
 require_once(ConnexionFolderPath."Connexion.php");
+require_once(UserFolderPath."User.php");
 
 require("libs/Smarty.class.php");
 
@@ -50,7 +51,26 @@ class UserService
 
 	// Modif utilisateur
 	private function put() {
-		return null;
+		$b = false;
+                parse_str(file_get_contents("php://input"),$post_vars);
+                if ($post_vars['password'] != '' && $post_vars['password'] == $post_vars['password_again'] && $post_vars['email'] != '' && $post_vars['firstname'] != '' && $post_vars['lastname'] != '') {
+                        $user = new User();
+                        $user->setEmail($post_vars['email']);
+                        $user->setFirstName($post_vars['firstname']);
+                        $user->setHashPwd($post_vars['password']);
+                        $user->setLastName($post_vars['lastname']);
+                        
+                        $_SESSION['acu_tck_login']='OK';
+			$_SESSION['user_email']=$user->getEmail();
+			$_SESSION['user_display_name']=$user->getFirstName().' '.$user->getLastName();
+                        
+                        $b = self::$_UserManager->add($user);
+		}
+                else
+                {
+                    $b = false;
+                }
+		return $b;
 	}
 
 	// Deconnexion utilisateur
